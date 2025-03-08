56d36e26381f04b486d5c5559d6e688193f05b01
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-03-08 11:40:37 +0800

    fix: fix unexpected end of comparison
    Testing the code, it was found out that only one pass happened throughout
    the method. Meaning, the code ended unexpectedly because the iteration
    reaches the last element of the array.
    
    Checking the array, only the largest number was sorted to the very end.
    To fix this, a loop was implemented so that each time an iteration is
    completed a value will be incremented until that value is greater than
    the array length.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,26 +1,27 @@
-arr = [4,3,78,2,0,2]
-
 def bubble_sorter(arr)
   p arr
+  i = 0
+
+  while i < arr.length - 1
     arr.each_with_index do |num, index|
       p num
       p arr
       p index
       if arr[index + 1 ] == nil
         next
-        if arr[index] > arr[index + 1]
-          a = arr[index]
-          b = arr[index +1]
-          arr[index] = b
-          arr[index + 1] = a
-          p arr[index]
-          p arr[index + 1]
-          p "this is #{num}"
-        end
+      elsif arr[index] > arr[index + 1]
+        a = arr[index]
+        b = arr[index +1]
+        arr[index] = b
+        arr[index + 1] = a
+        p arr[index]
+        p arr[index + 1]
+        p "this is #{num}"
       end
     end
+    i += 1
+  end
+  arr
 end
   
 bubble_sorter(arr)
-
-
