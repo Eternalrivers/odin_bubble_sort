fc13ec4c4ad58f1b0f8315181a53c719b84f3de6
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-03-09 19:04:18 +0800

    doc: clean code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,22 @@
 arr = [4,3,78,2,0,2,6]
 
 def bubble_sorter(arr)
-  p arr
   pass_count = 0
+
   until pass_count == arr.length - 1
     pass_count = 0
+
     arr.each_with_index do |num, index|
-      p "This is num#{num}"
-      p arr
-      p index
-       
       if arr[index + 1 ] == nil
         next
       elsif arr[index] > arr[index + 1]
         a = arr[index]
         b = arr[index +1]
+
         arr[index] = b
         arr[index + 1] = a
-        p arr[index]
-        p arr[index + 1]
-        p "this is #{num}"
       else
         pass_count += 1
-        p "this is pass count #{pass_count}"
         next
       end
     end
