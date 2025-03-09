86e4a5c35f9894d9e5f201abd5dcb838bbbdba3a
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-03-09 18:44:37 +0800

    fix: use a different loop to check if sorted
    Using a counter that checks for each completed iteration/pass will either
    make the sorter longer or shorter to complete.
    
    Like a  nearly sorted array or an array that needs only one pass to
    complete, it will mean that the method will continue the code execution
    until the counter reaches the set value.
    
    So a pass_count was implemented so that each pass will be counted every
    time a swap is not executed. A pass_count reaching the "length of an
    array minus 1" will mean the array was already sorted.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,15 @@
+arr = [4,3,78,2,0,2,6]
+
 def bubble_sorter(arr)
   p arr
-  i = 0
-
-  while i < arr.length - 1
+  pass_count = 0
+  until pass_count == arr.length - 1
+    pass_count = 0
     arr.each_with_index do |num, index|
-      p num
+      p "This is num#{num}"
       p arr
       p index
+       
       if arr[index + 1 ] == nil
         next
       elsif arr[index] > arr[index + 1]
@@ -17,9 +20,12 @@ def bubble_sorter(arr)
         p arr[index]
         p arr[index + 1]
         p "this is #{num}"
+      else
+        pass_count += 1
+        p "this is pass count #{pass_count}"
+        next
       end
     end
-    i += 1
   end
   arr
 end
